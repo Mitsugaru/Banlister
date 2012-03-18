141ae4c5513ecb63c367eb665ec87567b9996eb2
Rainfur <mudmander@gmail.com>
2012-03-17 21:41:27 -0700

    Update src/net/milkycraft/McbanFileReader.java

--- src/net/milkycraft/McbanFileReader.java
+++ src/net/milkycraft/McbanFileReader.java
@@ -6,36 +6,19 @@ import java.util.ArrayList;
 public class McbanFileReader extends ArrayList<String> {
 	
 	Charset charset = Charset.forName("US-ASCII");
+	String bannedpeople = new String[5000]; //syntax might not be right
 	
-	try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
-		String line = null;
+	
+	try (BufferedReader reader = Files.newBufferedReader("/mcbans/backup.txt", charset)) {
+		
+		String line;
+		int counter = 0;
+		
 		while((line = reader.readline()) != null) {
-			System.out.println(line); //Prints out the line, which you need to change to the .txt file, the entire filepath:
-			//ex: String line = "C:/Users/Erik/Documents/Data.txt"
-			//if not, try withoug the quotes
+			 bannedpeople[counter++] = line;	
 		}
 	} catch (IOException x) {
 		System.err.format("IOException: %s%n", x);
 	}
 
-}
-
-/* or you can try using
-regular files which
-are created, then updated on
-each method call, which I'll look
-up mcbans method for banning, 
-and i'll call that to write a string
-in the file
-*/
-
-Path file = ...;
-try {
-	//create the empty file with default permissions
-	Files.createFile(file);
-} catch (FileAlreadyExistsException x) {
-	System.err.format("file named %s" + " already exists%n", file);
-} catch (IOException x) {
-	//some other sort of fail
-	System.err.format("createFile error: %s%n", x);;
 }
